dd02266a8274e683dd283364c4e70f7ab6435d12
Colex317 <marsha.colex@gmail.com>
2024-05-30 02:04:53 -0500

    Adding files

---
+++ Dm_Prediction.sql
@@ -0,0 +1,17 @@
+-- Dm Prediction table:
+CREATE TABLE dm_prediction (
+	gender VARCHAR(10) NOT NULL,
+	age	DECIMAL NOT NULL,
+	hypertension INTEGER NOT NULL, 
+	heart_disease INTEGER NOT NULL, 	
+	smoking_history	VARCHAR(15) NOT NULL,
+	bmi	DECIMAL NOT NULL,
+	HbA1c_level	DECIMAL NOT NULL,
+	blood_glucose_level	INTEGER NOT NULL, 
+	diabetes INTEGER NOT NULL
+	);
+	
+
+SELECT *
+FROM dm_prediction
+;
